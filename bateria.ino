d4109e7905f1823ea3cc32ba97a61ae358953fa7
fkoteam <agl_2002@yahoo.es>
2020-09-29 00:20:05 +0200

    v3

--- bateria.ino
+++ bateria.ino
@@ -1,13 +1,19 @@
-#include <Arduino.h>
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
-#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>    
+#include <WiFiManager.h>
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
 #include <WiFiUdp.h>
+#include <analogmuxdemux.h>
 
+#include "der1cl.h"
+#include "der1op.h"
 #include "der2.h"
+#include "der3.h"
+#include "izq1.h"
 #include "izq2.h"
+#include "izq3.h"
+#include "pie_der.h"
+#include "pie_izq_cer.h"
 
 #include "AppleMIDI.h"
 USING_NAMESPACE_APPLEMIDI
@@ -23,17 +29,23 @@ APPLEMIDI_CREATE_INSTANCE(WiFiUDP, MIDI, "ESP8266", DEFAULT_CONTROL_PORT);
 
 // To run, set your ESP8266 build to 160MHz, and include a SPIFFS of 512KB or greater.
 
-unsigned long t0 = millis();
 
 
 AudioOutputI2SNoDAC *out;
-AudioOutputMixerStub *stub[2];
+AudioOutputMixerStub *stub[8];
 AudioOutputMixer *mixer;
-AudioGeneratorWAV *wav[2];
-AudioFileSourcePROGMEM *file[2];
-boolean Running[2];
-long start[2];
-long Play = 0;
+AudioGeneratorWAV *wav[8];
+AudioFileSourcePROGMEM *file[8];
+boolean Running[8];
+long start[8];
+
+AnalogMux amux(4, 3, 2, 0); //specifies the values for S0=4, S1=3, S2=2 on the 4051
+int valoresMaximos[8];
+
+int configMax[8];
+int configMin[8];
+
+
 
 
 int eepromInicializada = 0; //eeprom pos 1. Está a 1 si la placa está inicializada
@@ -48,16 +60,16 @@ void setup()
   delay(1000);
   leerEeprom(); //Lee las variables configurables de la Eeprom y si tienen valores inválidos, las inicializa
 
-WiFiManager wifiManager;
-wifiManager.autoConnect("bateria");
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("bateria");
 
   audioLogger = &Serial;
 
   out = new AudioOutputI2SNoDAC();
 
   mixer = new AudioOutputMixer(32, out);
-  stub[0] = mixer->NewInput();
-  stub[1] = mixer->NewInput();
+  for (int i = 0; i < 8; i++)
+    stub[i] = mixer->NewInput();
   delay(100);
 
 
@@ -65,65 +77,45 @@ wifiManager.autoConnect("bateria");
 
 
   initWifi();
-    Beginplay(1, der2,der2_len, 1.0);
-  Beginplay(0, izq2,izq2_len, 1.0);
-  Play = 1;
+  resetValoresMax();
 }
 
 void loop()
 {
-    MIDI.read();
-  if (wav[0]->isRunning()) {
-    if (!wav[0]->loop()) {
-      wav[0]->stop(); stub[0]->stop();
-      Running[0] = false; Serial.printf("stopping 0\n");
-    }
-  }
+  MIDI.read();
+  leerPiezos();
 
-  if (wav[1]->isRunning()) {
-    if (!wav[1]->loop()) {
-      wav[1]->stop();
-      stub[1]->stop();
-      Running[1] = false;
-      Serial.printf("stopping 1\n");
+  for (int i = 0; i < 8; i++)
+  {
+    if (wav[i]->isRunning()) {
+      if (!wav[i]->loop()) {
+        wav[i]->stop(); stub[i]->stop();
+        Running[i] = false; Serial.printf("stopping %d \n", i);
+      }
     }
   }
 
 
-  if (((millis() - start[1]) > 500) && (!wav[1]->isRunning())) {
-    if (!Running[1]) {
-      Play = Play + 1;
-      Serial.println(Play);
-      Beginplay(1, der2, der2_len, 1.0);
-    }
-  }
 
-  if (((millis() - start[0]) > 100) && (!wav[0]->isRunning())) {
-    if (!Running[0]) {
-      Play = Play + 1;
-      Serial.println(Play);
-      //Beginplay(0,"/piz11.wav", vol);
-      Beginplay(0, izq2, izq2_len, 1.0);
-    }
-  }
+
 }
 
 
 
 
-//inicializa el wifi 
+//inicializa el wifi
 void initWifi()
 {
   Serial.println("connected...yeey :)");
-  
+
   server.reset(new ESP8266WebServer(WiFi.localIP(), 80));
 
   server->on("/", handleRoot);
 
-    server->begin();                //Start server
+  server->begin();                //Start server
 
 
-while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(F("."));
   }
@@ -147,127 +139,57 @@ while (WiFi.status() != WL_CONNECTED) {
 //muestra la web de configuración y recoge los parámetros de configuración cuando se envían
 void handleRoot()
 {
-    String strRoot = "<!DOCTYPE HTML><html>\
-  <body>\
-  por hacer\
-  </body>\
-  </html>";
-  /*if (server.hasArg("dia") && server.hasArg("hora")) {
-    hora = getValue(server.arg("hora"), ':', 0).toInt(); // sino probar con %3A
-    minuto = getValue(server.arg("hora"), ':', 1).toInt();
-    dia = getValue(server.arg("dia"), '-', 2).toInt();
-    mes = getValue(server.arg("dia"), '-', 1).toInt();
-    anyo = getValue(server.arg("dia"), '-', 0).toInt();
 
-    setTime(hora, minuto, 0, dia, mes, anyo); // alternative to above, yr is 2 or 4 digit yr
-    digitalClockDisplay();
-  }
-  if (server.hasArg("MINUTOS_MUESTRA"))
+  for (int i = 0; i < 8; i++)
   {
-    MINUTOS_MUESTRA = server.arg("MINUTOS_MUESTRA").toInt();
-    EEPROM.put(50, MINUTOS_MUESTRA);
-    EEPROM.commit();
-    Serial.println("Nuevo parametro MINUTOS_MUESTRA: " + String(MINUTOS_MUESTRA));
-  }
-  if (server.hasArg("NUM_MUESTRAS")) {
-    NUM_MUESTRAS = server.arg("NUM_MUESTRAS").toInt();
-    EEPROM.put(100, NUM_MUESTRAS);
-    EEPROM.commit();
-    Serial.println("Nuevo parametro NUM_MUESTRAS: " + String(NUM_MUESTRAS));
-  }
-  if (server.hasArg("HUMEDAD_MIN")) {
-    HUMEDAD_MIN = server.arg("HUMEDAD_MIN").toInt();
-    EEPROM.put(60, HUMEDAD_MIN);
-    EEPROM.commit();
-    Serial.println("Nuevo parametro HUMEDAD_MIN: " + String(HUMEDAD_MIN));
+    if (server->hasArg("piezo_" + (String) i + "_max"))
+    {
+      int valor = server->arg("piezo_" + (String) i + "_max").toInt();
+      EEPROM.put(10 + (i * 10), valor);
+      EEPROM.commit();
+      Serial.println("Nuevo parametro piezo_" + (String) i + "_max: " + String(valor));
+      configMax[i]=valor;
+
+    }
+
+    if (server->hasArg("piezo_" + (String) i + "_min"))
+    {
+      int valor = server->arg("piezo_" + (String) i + "_min").toInt();
+      EEPROM.put(90 + (i * 10), valor);
+      EEPROM.commit();
+      Serial.println("Nuevo parametro piezo_" + (String) i + "_min: " + String(valor));
+      configMin[i]=valor;
+    }
+
   }
 
-  if (server.hasArg("SEGUNDOS_RIEGO")) {
-    SEGUNDOS_RIEGO = server.arg("SEGUNDOS_RIEGO").toInt();
-    EEPROM.put(70, SEGUNDOS_RIEGO);
-    EEPROM.commit();
-    Serial.println("Nuevo parametro SEGUNDOS_RIEGO: " + String(SEGUNDOS_RIEGO));
-  }
-  if (server.hasArg("DIAS_MAX")) {
-    DIAS_MAX = server.arg("DIAS_MAX").toInt();
-    EEPROM.put(80, DIAS_MAX);
-    EEPROM.commit();
-    Serial.println("Nuevo parametro DIAS_MAX: " + String(DIAS_MAX));
-  }
-  if (server.hasArg("HORAS_ENTRE_RIEGO_MIN")) {
-    HORAS_ENTRE_RIEGO_MIN = server.arg("HORAS_ENTRE_RIEGO_MIN").toInt();
-    EEPROM.put(90, HORAS_ENTRE_RIEGO_MIN);
-    EEPROM.commit();
-    Serial.println("Nuevo parametro HORAS_ENTRE_RIEGO_MIN: " + String(HORAS_ENTRE_RIEGO_MIN));
-  }
 
-  if (server.hasArg("accion") && server.arg("accion") == "regar")
-    regar();
+  if (server->hasArg("accion") && server->arg("accion") == "ResetMax")
+    resetValoresMax();
 
-  if (server.hasArg("accion") && server.arg("accion") == "compruebaHumedad")
-    compruebaHumedad();
 
-  String ultimosRiegos = getUltimosRiegos();
-  String ultimasHumedades = getUltimasHumedades();
-  char str[15] = "";
-  timeToString(str, sizeof(str));
+
   String strRoot = "<!DOCTYPE HTML><html>\
-  <body>\
-  <h1>Programa de riego</h1>\
-  Cada MINUTOS_MUESTRA, miramos la humedad. Tomamos NUM_MUESTRAS y hacemos la media para mayor precision. Si es inferior a HUMEDAD_MIN, regamos durante SEGUNDOS_RIEGO. Si han pasado DIAS_MAX desde el ultimo riego, regamos igualmente.\
-  Nos aseguramos que entre riego y riego pasan un minimo de HORAS_ENTRE_RIEGO_MIN.<br><br>\
-  Tiempo desde el ultimo encendido: " + String(str) + "<br>";
+    <body>\
+    <h1>ESP8288 BATERÍA</h1>";
+  for (int i = 0; i < 8; i++)
+  {
+    strRoot = strRoot + "<br><form>\
+    <label for=\"fname\">PIEZO " + (String) i + " (Máx actual: " + (String)valoresMaximos[i] + ") </label>\
+    Umbral Min/Máx: <input type=\"number\" style=\"width: 4em;\" min=\"1\" max=\"1024\" id=\"piezo_" + (String) i + "_min\" name=\"piezo_" + (String) i + "_min\" value=\"" +  (String)configMin[i] + "\">\
+    <input type=\"number\" style=\"width: 4em;\" min=\"1\" max=\"1024\" id=\"piezo_" + (String) i + "_max\" name=\"piezo_" + (String) i + "_max\" value=\"" +  (String)configMax[i] + "\">\
+    <input type=\"submit\" value=\"Envia\"><br>\
+  </form><br>";
+  }
 
-  strRoot += "              <form>\
-                   <label for = \"fname\">Fecha y hora:</label>\
-    <input type=\"date\" id=\"dia\" name=\"dia\" value=\"" + String(year()) + "-" + transformaAdosDigitos(month()) + "-" + transformaAdosDigitos(day()) + "\">\
-    <input type=\"time\" id=\"hora\" name=\"hora\" value=\"" + transformaAdosDigitos(hour()) + ":" + transformaAdosDigitos(minute()) + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form> \
-  \
-  <form>\
-    <label for=\"fname\">MINUTOS_MUESTRA:</label>\
-    <input type=\"number\" style=\"width: 3em;\" min=\"1\" max=\"59\" id=\"MINUTOS_MUESTRA\" name=\"MINUTOS_MUESTRA\" value=\"" +  MINUTOS_MUESTRA + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form> \
-  <form>\
-    <label for=\"fname\">NUM_MUESTRAS:</label>\
-    <input type=\"number\" style=\"width: 3em;\" min=\"1\" max=\"100\" id=\"NUM_MUESTRAS\" name=\"NUM_MUESTRAS\" value=\"" + NUM_MUESTRAS + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form>\
-  <form>\
-    <label for=\"fname\">HUMEDAD_MIN:</label>\
-    <input type=\"number\" style=\"width: 4em;\" min=\"1\" max=\"1000\" id=\"HUMEDAD_MIN\" name=\"HUMEDAD_MIN\" value=\"" + HUMEDAD_MIN + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form>\
-  <form>\
-    <label for=\"fname\">SEGUNDOS_RIEGO:</label>\
-    <input type=\"number\" style=\"width: 3em;\" min=\"1\" max=\"200\" id=\"SEGUNDOS_RIEGO\" name=\"SEGUNDOS_RIEGO\" value=\"" + SEGUNDOS_RIEGO + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form>\
-  <form>\
-    <label for=\"fname\">DIAS_MAX:</label>\
-    <input type=\"number\" style=\"width: 3em;\" min=\"1\" max=\"30\" id=\"DIAS_MAX\" name=\"DIAS_MAX\" value=\"" + DIAS_MAX + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form>\
-  <form>\
-    <label for=\"fname\">HORAS_ENTRE_RIEGO_MIN:</label>\
-    <input type=\"number\" style=\"width: 3em;\" min=\"1\" max=\"300\" id=\"HORAS_ENTRE_RIEGO_MIN\" name=\"HORAS_ENTRE_RIEGO_MIN\" value=\"" + HORAS_ENTRE_RIEGO_MIN + "\">\
-    <input type=\"submit\" value=\"Envia\"><br>\
-  </form>\
-  Ultimas mediciones humedad: " + ultimasHumedades + "<br>\
-  Ultimos riegos: " + ultimosRiegos + "<br>\
-  <form>\
-    <button name=\"accion\" type=\"submit\" value=\"regar\">Regar ahora</button><br>\
-  </form> \
-  <form>\
-    <button name=\"accion\" type=\"submit\" value=\"compruebaHumedad\">Comprobar humedad</button><br>\
-  </form> \
-  </body>\
-  </html>";
-*/
+  strRoot = strRoot + "<form>\
+    <button name=\"accion\" type=\"submit\" value=\"ResetMax\">Resetea máximos</button><br>\
+    </form> \
+    </body>\
+    </html>";
 
-    server->send(200, "text/html", strRoot);
+
+  server->send(200, "text/html", strRoot);
 
 }
 
@@ -276,9 +198,9 @@ void inicializarEeprom()
 {
   Serial.println("Inicializando Eeprom, tardaremos 10 segundos...");
 
-  for (int i = 0; i < 10; i++)
+  for (int i = 0; i < 16; i++)
   {
-    EEPROM.put(10+(i * 10), i);
+    EEPROM.put(10 + (i * 10), i);
     EEPROM.commit();
 
   }
@@ -289,63 +211,31 @@ void inicializarEeprom()
 //Lee las variables configurables de la Eeprom y si tienen valores inválidos, las inicializa
 void leerEeprom()
 {
- /* EEPROM.get(50, MINUTOS_MUESTRA);
-  if (MINUTOS_MUESTRA < 1 || MINUTOS_MUESTRA > 59)
+  for (int i = 0; i < 16; i++)
   {
-    MINUTOS_MUESTRA = 5;
-    EEPROM.put(50, MINUTOS_MUESTRA);
-    EEPROM.commit();
+    int valor = 0;
+    EEPROM.get(10 + (i * 10), valor);
+    if (valor < 1 || valor > 1024)
+    {
+      valor = 512;
+      EEPROM.put(10 + (i * 10), valor);
+      EEPROM.commit();
+    }
+    if (i < 8)
+    {
+      configMax[i] = valor;
+    }
+    else
+    {
+      configMin[i - 8] = valor;
+
+    }
+
+
   }
-  EEPROM.get(60, HUMEDAD_MIN);
-  if (HUMEDAD_MIN < 1 || HUMEDAD_MIN > 1000)
-  {
-    HUMEDAD_MIN = 730;
-    EEPROM.put(60, HUMEDAD_MIN);
-    EEPROM.commit();
-  }
-  EEPROM.get(70, SEGUNDOS_RIEGO);
-  if (SEGUNDOS_RIEGO < 1 || SEGUNDOS_RIEGO > 200)
-  {
-    SEGUNDOS_RIEGO = 60;
-    EEPROM.put(70, SEGUNDOS_RIEGO);
-    EEPROM.commit();
-  }
-  EEPROM.get(80, DIAS_MAX);
-  if (DIAS_MAX < 1 || DIAS_MAX > 30)
-  {
-    DIAS_MAX = 4;
-    EEPROM.put(80, DIAS_MAX);
-    EEPROM.commit();
-  }
-  EEPROM.get(90, HORAS_ENTRE_RIEGO_MIN);
-  if (HORAS_ENTRE_RIEGO_MIN < 1 || HORAS_ENTRE_RIEGO_MIN > 300)
-  {
-    HORAS_ENTRE_RIEGO_MIN = 24;
-    EEPROM.put(90, HORAS_ENTRE_RIEGO_MIN);
-    EEPROM.commit();
-  }
-  EEPROM.get(100, NUM_MUESTRAS);
-  if (NUM_MUESTRAS < 1 || NUM_MUESTRAS > 100)
-  {
-    NUM_MUESTRAS = 10;
-    EEPROM.put(100, NUM_MUESTRAS);
-    EEPROM.commit();
-  }
-  EEPROM.get(110, contador_muestras_humedad);
-  if (contador_muestras_humedad < 0 || contador_muestras_humedad > 9)
-  {
-    contador_muestras_humedad = 0;
-    EEPROM.put(110, contador_muestras_humedad);
-    EEPROM.commit();
-  }
-  EEPROM.get(120, contador_riegos);
-  if (contador_riegos < 0 || contador_riegos > 9)
-  {
-    contador_riegos = 0;
-    EEPROM.put(120, contador_riegos);
-    EEPROM.commit();
-  }
-*/
+
+
+
   EEPROM.get(1, eepromInicializada);
   if (eepromInicializada != 1)
   {
@@ -357,7 +247,7 @@ void leerEeprom()
 }
 
 
-void Beginplay(int Channel, const void *wavfilename, int sizewav,float Volume) {
+void Beginplay(int Channel, const void *wavfilename, int sizewav, float Volume, byte note) {
   // String Filename;
   // Filename=wavfilename;
   Serial.printf("CH:");
@@ -368,8 +258,8 @@ void Beginplay(int Channel, const void *wavfilename, int sizewav,float Volume) {
 
   delete file[Channel]; // housekeeping ?
 
-  file[Channel] = new AudioFileSourcePROGMEM( wavfilename, sizewav ); 
- 
+  file[Channel] = new AudioFileSourcePROGMEM( wavfilename, sizewav );
+
   wav[Channel]->begin(file[Channel], stub[Channel]);
 
   Running[Channel] = true;
@@ -377,10 +267,54 @@ void Beginplay(int Channel, const void *wavfilename, int sizewav,float Volume) {
   Serial.println(Volume);
   start[Channel] = millis();
 
-      byte note = 45;
-    byte velocity = 55;
-    byte channel = 1;
+  byte velocity = Volume*127;
+  byte channel = 1;
 
-    MIDI.sendNoteOn(note, velocity, channel);
-    MIDI.sendNoteOff(note, velocity, channel);
+  MIDI.sendNoteOn(note, velocity, channel);
+  MIDI.sendNoteOff(note, velocity, channel);
+}
+
+void leerPiezos() {
+  for (int i = 0; i < 8; i++)
+  {
+    int val = amux.AnalogRead(i);
+    if (valoresMaximos[i] < val)
+      valoresMaximos[i] = val;
+    if (configMin[i] < val)
+    {
+      if (i == 0)
+        Beginplay(0, der1cl, der1cl_len, val/configMax[i],1);//  TODO CLOSE / OPEN
+      else if (i == 1)
+        Beginplay(1, der2, der2_len, val/configMax[i],10);
+      else if (i == 2)
+        Beginplay(2, der3, der3_len, val/configMax[i],20);
+      else if (i == 3)
+        Beginplay(3, izq1, izq1_len, val/configMax[i],30);
+      else if (i == 4)
+        Beginplay(4, izq2, izq2_len, val/configMax[i],40);
+      else if (i == 5)
+        Beginplay(5, izq3, izq3_len, val/configMax[i],50);
+      else if (i == 6)
+        Beginplay(6, pie_der, pie_der_len, val/configMax[i],60);
+      else if (i == 7)
+        Beginplay(7, pie_izq_cer, pie_izq_cer_len, val/configMax[i],70);
+      if (i == 7)//ARREGLAR
+        Beginplay(7, der1op, der1op_len, val/configMax[i],70);//ARREGLAR
+
+
+
+
+    }
+
+
+  }
+}
+
+void resetValoresMax() {
+  for (int i = 0; i < 8; i++)
+  {
+    valoresMaximos[i] = 0;
+
+
+  }
 }
